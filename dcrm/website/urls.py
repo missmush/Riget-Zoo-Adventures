4d7e8f8fde288e336660380524387fcc90483de9
bridgetshroom <160240629@student.rnngroup.ac.uk>
2024-10-22 14:44:51 +0100

    hii yipee

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home,name=''),
 
-    path('about', views.about,name='about')
+    path('about', views.about,name='about'),
 
+    path('zoo-booking', views.zoobooking,name='zoo-booking'),
+
+    path('hotel-booking', views.hotelbooking,name='hotel-booking'),
+
+    path('adoption', views.adopt,name='adoption'),
 ]
