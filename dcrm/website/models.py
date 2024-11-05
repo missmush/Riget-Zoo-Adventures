0502c2b1d6188c8a5ca72111aeb253f60d4f5f43
bridgetshroom <160240629@student.rnngroup.ac.uk>
2024-11-05 12:11:18 +0000

    added icons and text

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class ZooUser(AbstractUser):
+    points = models.IntegerField(default=0)
+    address = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=14, blank=True)
